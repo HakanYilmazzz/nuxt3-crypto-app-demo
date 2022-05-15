71d65de0e966810aaad2d507272e92d5d1e63bd2
HakanYilmazzz <hakanyilmazzz93@gmail.com>
2022-05-15 22:22:18 +0300

    Price typo changes

--- components/fetch.vue
+++ components/fetch.vue
@@ -2,7 +2,10 @@
 const contents = useState('contents', () => null);
 async function fetchData() {
     try {
-        const { data } = await useFetch(`https://api.nomics.com/v1/currencies/ticker?key=1da999db0f7e0fa4f70d97f48d6ffe612260cf18&interval=1d,30d&convert=TRY`); // prettier-ignore
+        const { data } = await useFetch(`https://api.nomics.com/v1/currencies/ticker?key=1da999db0f7e0fa4f70d97f48d6ffe612260cf18&interval=1d,30d&convert=TRY`,{
+            method: "GET",
+            credentials:"same-origin",
+        }); // prettier-ignore
         if (!data) throw new Error('cannot fetch data');
         contents.value = data;
     } catch (error) {
@@ -32,14 +35,14 @@ async function fetchData() {
                     </thead>
                     <tbody class="p-2 border-solid border-1  bg-white  text-center ">
                         <tr v-for="(content, index) in contents" class="shadow-lg shadow-cyan-500/50">
-                            <td class=" rounded-l-lg"><img :src="content.logo_url" alt="" width="70"
+                            <td class=" rounded-l-lg"><img :src="content.logo_url" alt="" width="60"
                                     style="margin-left:25%"></td>
                             <td style="color:purple !important">{{ content.name }}<br><span
                                     class="text-white text-sm w-1/3 pb-1 bg-green-600 font-semibold px-2 rounded-full">{{
                                             content.symbol
                                     }}</span>
                             </td>
-                            <td class="rounded-r-lg">{{ parseFloat(content.price).toFixed(3) }} TL</td>
+                            <td class="rounded-r-lg">{{ parseFloat(content.price).toLocaleString() }} TL</td>
                         </tr>
                     </tbody>
                 </table>
